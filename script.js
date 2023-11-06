7a508bbe314d4a4b5b0ef9e093494479cd614e00
satishvatturu <93177666+satishvatturu@users.noreply.github.com>
2023-11-06 18:17:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let bodyEle = document.body; 
+document.addEventListener("DOMContentLoaded", (e)=>{
+  let element = document.createElement("p");
+  element.innerText = "DOM load success";
+  bodyEle.appendChild(element);
+})
